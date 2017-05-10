b5ad8779924fb8d4a1acf29b4e5726bd3b05f7b2
Chainn <578579786@qq.com>
2017-05-10 22:00:32 +0800

    Add pm2

--- node-lessons/cluster/app.js
+++ node-lessons/cluster/app.js
@@ -23,3 +23,16 @@ if (cluster.isMaster) {
 } else {
     require("./index.js");
 }
+
+/**
+ * pm2:
+ * pm2 start filename
+ * pm2 stop all
+ * pm2 list
+ * pm2 restart all
+ * pm2 stop [app-name|id]  #停止某一个进程，可以使用app-name或者id
+ * pm2 delete [app-name|id]#删除并停止进程
+ * pm2 logs
+ * pm2 web
+ * pm2 monit 涉及重启次数、运行时间、脚本路径、参数、日志路径、运行模式等等信息
+ * **/
