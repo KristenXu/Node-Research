4c5fb1807f7280d747baf190c3a6b0232ab1b681
Chainn <578579786@qq.com>
2017-05-11 10:03:19 +0800

    Add siege for Stress Testing load balance on on machine; Add message communication between master and worker

--- node-lessons/cluster/app.js
+++ node-lessons/cluster/app.js
@@ -1,27 +1,34 @@
 var cluster = require('cluster');
+var http = require('http');
 var os = require('os');
-
 const CPUS = os.cpus();
+console.log('CPUS', CPUS.length)
 
 if (cluster.isMaster) {
     CPUS.forEach(function () {
         cluster.fork();
     });
-    cluster.on('listening', function(worker) {
-        console.log('Cluster %d connected', worker.process.pid);
+    cluster.on('listening', function(worker, address) {
+        console.log(address)
+        console.log('worker %d connected', worker.process.pid, ' , address: ' + address.address, ' , port: ' + address.port);
     });
 
-    cluster.on('disconnect', function(worker) {
-        console.log('Cluster %d disconnected', worker.process.pid);
+    cluster.on('disconnect', function(worker, address) {
+        console.log('worker %d disconnected', worker.process.pid, ' , port: ' + address.port);
     });
+
 
     cluster.on('exit', function(worker) {
-        console.log('Cluster %d dead', worker.process.pid);
+        console.log('worker %d dead', worker.process.pid);
         // Ensuring a new cluster will start if an old one dies
         cluster.fork();
     });
 } else {
-    require("./index.js");
+    console.log('[worker] ' + "start worker ..." + cluster.worker.id);
+    http.createServer(function (req, res) {
+        console.log('worker'+cluster.worker.id);
+        res.end('worker'+cluster.worker.id+',PID:'+process.pid);
+    }).listen(3002);
 }
 
 /**
@@ -36,3 +43,12 @@ if (cluster.isMaster) {
  * pm2 web
  * pm2 monit 涉及重启次数、运行时间、脚本路径、参数、日志路径、运行模式等等信息
  * **/
+
+/**
+ *
+ *node server.js > server.log
+ * siege -c 50 http://localhost:3000 运行siege启动命令进行压力测试,每秒50个并发请求。
+ * ~ R
+ > df<-read.table(file="server.log",skip=20,header=FALSE)
+ > summary(df)
+ **/
