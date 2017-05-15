b60c3c941d382e5cc5c65905d759d50a5ee1748c
Chainn <578579786@qq.com>
2017-05-15 21:48:07 +0800

    Add hello wold request

---
+++ node-in-action/Chapter1-welcome/Helloworld-request.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var server = http.createServer();
+server.on('request', function (req, res) {
+    res.writeHead(200,{'Content-Type': 'text/plain'});
+    res.end('Hello World \n')
+});
+server.listen(3000);
+console.log('server running on at http://locahost:3000/')
