16bbd8f9228f6666f76731337bb5c9aafbdb5b10
Chainn <578579786@qq.com>
2017-05-15 21:55:29 +0800

    Add streaming data

---
+++ node-in-action/Chapter1-welcome/streamingData.js
@@ -0,0 +1,9 @@
+
+var fs = require('fs')
+var stream = fs.createReadStream('./resource.json')
+stream.on('data', function (chunk) {
+    console.log(chunk)
+})
+stream.on('end',function () {
+    console.log('end')
+})
