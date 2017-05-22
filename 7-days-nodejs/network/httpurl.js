c9dc6c4887894fc553fb03f709230a6a7d5571e0
Chainn <578579786@qq.com>
2017-05-22 09:50:39 +0800

    Fix query string.parse

--- 7-days-nodejs/network/httpurl.js
+++ 7-days-nodejs/network/httpurl.js
@@ -5,20 +5,24 @@ http.createServer(function (request, response) {
     var tmp = request.url; // => "/foo/bar?a=b"
     var parsedUrl = url.parse(tmp);
     console.log('parsedUrl', parsedUrl)
-    var queryS = querystring.parse(parsedUrl.query);
-    console.log('queryS', queryS)
     /* =>
-     { protocol: null,
+     {
+     protocol: null,
      slashes: null,
      auth: null,
      host: null,
      port: null,
      hostname: null,
      hash: null,
-     search: '?a=b',
-     query: 'a=b',
-     pathname: '/foo/bar',
-     path: '/foo/bar?a=b',
-     href: '/foo/bar?a=b' }
+     search: '?test=11&hh=33&hh=34',
+     query: 'test=11&hh=33&hh=34',
+     pathname: '/',
+     path: '/?test=11&hh=33&hh=34',
+     href: '/?test=11&hh=33&hh=34' }
+     */
+    var queryS = querystring.parse(parsedUrl.query);
+    console.log('queryS', queryS)
+    /*
+     { test: '11', hh: [ '33', '34' ] }
      */
 }).listen(3000);
