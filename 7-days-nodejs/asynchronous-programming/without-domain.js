8e77e036b60ab560ebf7ffea10366ed57ee9dc60
Chainn <578579786@qq.com>
2017-05-23 10:15:50 +0800

    Add without domain

---
+++ 7-days-nodejs/asynchronous-programming/without-domain.js
@@ -0,0 +1,37 @@
+function async(request, callback) {
+    // Do something.
+    asyncA(request, function (err, data) {
+        if (err) {
+            callback(err);
+        } else {
+            // Do something
+            asyncB(request, function (err, data) {
+                if (err) {
+                    callback(err);
+                } else {
+                    // Do something
+                    asyncC(request, function (err, data) {
+                        if (err) {
+                            callback(err);
+                        } else {
+                            // Do something
+                            callback(null, data);
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+http.createServer(function (request, response) {
+    async(request, function (err, data) {
+        if (err) {
+            response.writeHead(500);
+            response.end();
+        } else {
+            response.writeHead(200);
+            response.end(data);
+        }
+    });
+});
