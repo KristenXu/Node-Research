e9a9bc72dc8d57b41a004709f77963a5206fb121
Chainn <578579786@qq.com>
2017-05-23 10:16:48 +0800

    Add sync error

---
+++ 7-days-nodejs/asynchronous-programming/sync-error.js
@@ -0,0 +1,14 @@
+function main() {
+    // Do something.
+    syncA();
+    // Do something.
+    syncB();
+    // Do something.
+    syncC();
+}
+
+try {
+    main();
+} catch (err) {
+    // Deal with exception.
+}
