52ae5d0b394068b5289b8163f3921fcfa1bf0d5b
Chainn <578579786@qq.com>
2017-06-09 23:25:05 +0800

    Add express

---
+++ tutorial/Express/express_demo.js
@@ -0,0 +1,22 @@
+//express_demo.js 文件
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
+
+app.get('/hello', function (req, res) {
+    console.log('req', req.path)
+    res.send('hello')
+    // --
+})
