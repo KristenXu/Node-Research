ce0b4692d2bfa729ad53314841ee94722e98866d
Chainn <578579786@qq.com>
2017-06-10 22:20:02 +0800

    Add upload file

--- tutorial/Express/express_demo.js
+++ tutorial/Express/express_demo.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var fs = require("fs");
+var multer  = require('multer');
 
 app.get('/', function (req, res) {
     res.send('Hello World');
@@ -29,7 +31,7 @@ app.get('/ab*cd', function(req, res) {
 });
 app.get('/index', function (req, res) {
     res.sendFile(__dirname + '/index.html');
-})
+});
 app.get('/process_get', function (req, res) {
 
     // 输出 JSON 格式
@@ -39,7 +41,7 @@ app.get('/process_get', function (req, res) {
     };
     console.log(response);
     res.end(JSON.stringify(response));
-})
+});
 
 app.post('/process_post', urlencodedParser, function (req, res) {
 
@@ -50,4 +52,25 @@ app.post('/process_post', urlencodedParser, function (req, res) {
     };
     console.log(response);
     res.end(JSON.stringify(response));
+});
+
+app.post('/file_upload', function (req, res) {
+
+    console.log(req.files[0]);  // 上传的文件信息
+
+    var des_file = __dirname + "/" + req.files[0].originalname;
+    fs.readFile( req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if( err ){
+                console.log( err );
+            }else{
+                response = {
+                    message:'File uploaded successfully',
+                    filename:req.files[0].originalname
+                };
+            }
+            console.log( response );
+            res.end( JSON.stringify( response ) );
+        });
+    });
 })
