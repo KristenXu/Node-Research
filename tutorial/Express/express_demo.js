6d25bb7ce4ca3d53f46b71d58c08878b7dbedf0d
Chainn <578579786@qq.com>
2017-06-10 22:13:19 +0800

    Add express POST

--- tutorial/Express/express_demo.js
+++ tutorial/Express/express_demo.js
@@ -1,6 +1,8 @@
 //express_demo.js 文件
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', function (req, res) {
     res.send('Hello World');
@@ -25,13 +27,26 @@ app.get('/ab*cd', function(req, res) {
     console.log("/ab*cd GET 请求");
     res.send('正则匹配');
 });
-
+app.get('/index', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
 app.get('/process_get', function (req, res) {
 
     // 输出 JSON 格式
     var response = {
         "first_name":req.query.first_name,
         "last_name":req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+
+    // 输出 JSON 格式
+    var response = {
+        "first_name":req.body.first_name,
+        "last_name":req.body.last_name
     };
     console.log(response);
     res.end(JSON.stringify(response));
