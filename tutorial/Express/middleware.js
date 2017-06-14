84952f39bc710ad8d6f4489f3e9e238898fa1ff9
Chainn <578579786@qq.com>
2017-06-14 22:48:41 +0800

    Add router level middleware

--- tutorial/Express/middleware.js
+++ tutorial/Express/middleware.js
@@ -1,5 +1,6 @@
 var express = require('express')
-var app = express()
+var app = express();
+var router = express.Router()
 
 var requestTime = function (req, res, next) {
     req.requestTime = Date.now()
@@ -17,6 +18,7 @@ app.get('/', function (req, res) {
 })
 
 // Application level
+//The function is executed every time the app receives a request.
 app.use(function (req, res, next) {
     console.log('Time:', Date.now())
     next()
@@ -30,5 +32,42 @@ app.use('/user/:id', function (req, res, next) {
 app.get('/user/:id', function (req, res, next) {
     res.send('USER')
 })
+
+
+// Router level
+// a middleware function with no mount path. This code is executed for every request to the router
+router.use(function (req, res, next) {
+    console.log('Time:', Date.now())
+    next()
+})
+
+// a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
+router.use('/user/:id', function (req, res, next) {
+    console.log('Request URL:', req.originalUrl)
+    next()
+}, function (req, res, next) {
+    console.log('Request Type:', req.method)
+    next()
+})
+
+// a middleware sub-stack that handles GET requests to the /user/:id path
+router.get('/user/:id', function (req, res, next) {
+    // if the user ID is 0, skip to the next router
+    if (req.params.id === '0') next('route')
+    // otherwise pass control to the next middleware function in this stack
+    else next()
+}, function (req, res, next) {
+    // render a regular page
+    res.render('regular')
+})
+
+// handler for the /user/:id path, which renders a special page
+router.get('/user/:id', function (req, res, next) {
+    console.log(req.params.id)
+    res.render('special')
+})
+
+// mount the router on the app
+app.use('/', router)
 
 app.listen(3000)
