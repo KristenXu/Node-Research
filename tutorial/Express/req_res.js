848b79739551eda0f9a475aeee51d5cffd3b8230
Chainn <578579786@qq.com>
2017-06-19 16:05:06 +0800

    Add post for express

--- tutorial/Express/req_res.js
+++ tutorial/Express/req_res.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+app.use(require('body-parser')());
 app.get('/test_header/:id', function (req, res) {
     console.log('req', req)
     res.set('Content-Type', 'text/plain');
@@ -8,5 +8,22 @@ app.get('/test_header/:id', function (req, res) {
     for(var name in req.headers) s += name + ': ' + req.headers[name] + '\n';
     res.send(s)
 });
+
+app.post('/process_post', function (req, res) {
+    console.log('body', req.body);
+    res.send('post succeed!')
+});
+
+
+//without bodyparser
+// app.post('/process_post', function (req, res) {
+//     console.log('body', req.body);
+//     var data = ""
+//     req.on('data', function(chunk){ data += chunk})
+//     req.on('end', function(){
+//         console.log('sss', data)
+//     })
+//     res.send('post succeed!')
+// });
 app.disable('x-powered-by');
 app.listen(3000)
