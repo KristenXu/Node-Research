b02d2ccbec61ab04aa77cf43ee987a06f4ff4b7e
Chainn <578579786@qq.com>
2017-06-22 22:47:03 +0800

    Add keep-alive for comet

--- node-in-action/Chapter1-welcome/Helloworld.js
+++ node-in-action/Chapter1-welcome/Helloworld.js
@@ -1,6 +1,6 @@
 var http = require('http')
 http.createServer(function (req, res) {
-    res.writeHead(200,{'Content-Type': 'text/plain'});
+    res.writeHead(200,{'Content-Type': 'text/plain',Connection:keep-alive});
     res.end('Hello World \n')
 }).listen(3000);
 console.log('Server running at http://localhost:3000')
